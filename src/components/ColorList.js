7dbec4826c2f68375a7c69eb26af0e495e27da61
OsipciucIon19 <ion-osipciuc@mail.ru>
2021-11-16 14:44:58 +0200

    add change state with context

---
+++ src/components/ColorList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Color from "./Color.js"
+
+export default function ColorList({ colors = [], onRemoveColor = f => f, onRateColor = f => f }) {
+    if (!colors.length) return <div>No colors listed.</div>;
+    return (
+        <div>
+            {
+                colors.map(color => (
+                    <Color key={color.id} {...color} onRemove={onRemoveColor} onRate={onRateColor}/>
+                ))
+            }
+        </div>
+    )
+}
