75b70b83af1df79f5372f6afb9a395e7629be95a
OsipciucIon19 <ion-osipciuc@mail.ru>
2021-11-16 09:28:26 +0200

    add color form

---
+++ src/StarRating.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { FaStar } from "react-icons/fa"
+
+function App() {
+  return (
+    <>
+      <FaStar color="red"/>
+      <FaStar color="grey"/>
+      <FaStar color="grey"/>
+      <FaStar color="grey"/>
+    </>
+  );
+}
+
+export default App;
